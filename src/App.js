adaee2e189183b6d5a218322aab28163ecfc6827
Smiley <jcsmileyjr@gmail.com>
2020-07-18 08:32:19 -0500

    Refactor to save save user input to component state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 //import axios from 'axios';
 
 function App() {
+  const [userInput, setUserInput] = useState(""); // User entered data retrived from text area
 
+  // Function to submit the user inputed question to the back end.
   const onSubmit = () => {
+
     alert("hello");
-  } 
+  }
+  
+  // Function to saved user input to the component state
+  const captureUserInput = e => {
+    setUserInput(e.target.value);
+  }
+
   return (
     <div>
       <h1 className="header">Code Connector</h1>
       <section className="content-container">
         <div className="content">
         <h3 className="input-title">Submit a Daily Question</h3>
-        <textarea type="text" className="input-style" placeholder="Write question here ....." rows="100" cols="30" />
+        <textarea 
+          type="text" 
+          className="input-style" 
+          placeholder="Write question here ....." 
+          rows="100" 
+          cols="30" 
+          onChange={captureUserInput}
+        />
         <button type="button" className="button-style" onClick={onSubmit}>Submit</button>
         </div>
       </section>
