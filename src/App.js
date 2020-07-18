3663465aeaab72822f2738068be21e9b5f793567
Smiley <jcsmileyjr@gmail.com>
2020-07-18 07:23:50 -0500

    Refactor text area to show message to user

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
 
   const onSubmit = () => {
     alert("hello");
-  }
+  } 
   return (
     <div>
       <h1 className="header">Code Connector</h1>
       <section className="content-container">
         <div className="content">
         <h3 className="input-title">Submit a Daily Question</h3>
-        <textarea type="text" className="input-style" rows="100" cols="30" />
+        <textarea type="text" className="input-style" placeholder="Write question here ....." rows="100" cols="30" />
         <button type="button" className="button-style" onClick={onSubmit}>Submit</button>
         </div>
       </section>
